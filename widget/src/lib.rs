4fada660b7e849565b4e985edd9e4fd7be4dbf35
Elia Bieri <contact@eliabieri.com>
2023-05-22 17:57:22 +0200

    Initial implementation

--- widget/src/lib.rs
+++ widget/src/lib.rs
@@ -1,20 +1,54 @@
 use serde_json::Error;
-use serde_json::Value;
 
 use schemars::{schema_for, JsonSchema};
 use serde::Deserialize;
+use time::format_description;
+use time::OffsetDateTime;
+use time_humanize::Accuracy;
+use time_humanize::HumanTime;
+use time_humanize::Tense;
 
 wit_bindgen::generate!({
     path: "../wg_display_widget_wit/wit",
     world: "widget"
 });
 
-#[derive(JsonSchema, Deserialize)]
-struct WidgetConfig {
-    city: String,
+#[derive(Deserialize)]
+struct FromData {
+    #[serde(with = "time::serde::iso8601")]
+    departure: OffsetDateTime,
 }
 
-const WIDGET_NAME: &str = "Rust Widget Template";
+#[derive(Deserialize)]
+struct FromMetaData {
+    name: String,
+}
+
+#[derive(Deserialize)]
+struct ToMetaData {
+    name: String,
+}
+
+#[derive(Deserialize)]
+struct ConnectionData {
+    from: FromData,
+}
+
+#[derive(Deserialize)]
+struct PublicTransportData {
+    connections: Vec<ConnectionData>,
+    from: FromMetaData,
+    to: ToMetaData,
+}
+
+#[derive(JsonSchema, Deserialize)]
+struct WidgetConfig {
+    from_station: String,
+    to_station: String,
+    num_connections: u8,
+}
+
+const WIDGET_NAME: &str = "Public Transport";
 
 struct MyWidget;
 
@@ -23,31 +57,23 @@ impl Widget for MyWidget {
         WIDGET_NAME.into()
     }
 
-    fn run(config: WidgetContext) -> WidgetResult {
-        // Widgets can log to the console
-        logging::log(logging::Level::Info, WIDGET_NAME, "Widget run started");
-
-        // Widgets can handle the case where no config is provided
-        if "{}" == config.config {
+    fn run(context: WidgetContext) -> WidgetResult {
+        if "{}" == context.config {
             return WidgetResult {
                 data: "No config provided".into(),
             };
         }
 
-        // Widgets can parse their config with ease using serde
         let config: WidgetConfig =
-            serde_json::from_str(&config.config).expect("Failed to parse config");
+            serde_json::from_str(&context.config).expect("Failed to parse config");
 
-        // Widgets can make network requests
-        let response = http::request(
-            http::Method::Get,
-            format!(
-                "https://aareguru.existenz.ch/v2018/today?city={}",
-                config.city
-            )
-            .as_str(),
-            None,
+        let url = format!(
+            "http://transport.opendata.ch/v1/connections?from={}&to={}&limit=16",
+            urlencoding::encode(config.from_station.as_str()),
+            urlencoding::encode(config.to_station.as_str()),
         );
+
+        let response = http::request(http::Method::Get, url.as_str(), None);
         let Ok(response) = response else {
             return WidgetResult {
                 data: "Failed to make network request".into(),
@@ -60,13 +86,17 @@ impl Widget for MyWidget {
             };
         }
 
-        let data: Result<Value, Error> = serde_json::from_slice(response.bytes.as_slice());
-        let result = match data {
-            Ok(data) => format!("Aare Temperature in {}: {} C", config.city, data["aare"]),
-            Err(_) => "Response from AareGuru could not be parsed".into(),
+        let data: Result<PublicTransportData, Error> =
+            serde_json::from_slice(response.bytes.as_slice());
+        if let Err(error) = data {
+            return WidgetResult {
+                data: format!("Failed to parse response: {:?}", error),
+            };
         };
+        let data = data.unwrap();
+        let content = MyWidget::get_departure_string(&data, config.num_connections as usize);
 
-        WidgetResult { data: result }
+        WidgetResult { data: content }
     }
 
     fn get_config_schema() -> wit_bindgen::rt::string::String {
@@ -76,6 +106,56 @@ impl Widget for MyWidget {
 
     fn get_version() -> wit_bindgen::rt::string::String {
         "1.0.0".into()
+    }
+
+    fn get_run_update_cycle_seconds() -> u32 {
+        90
+    }
+}
+
+impl MyWidget {
+    pub fn now() -> OffsetDateTime {
+        let now = clocks::now();
+        OffsetDateTime::from_unix_timestamp(now.seconds as i64).unwrap()
+    }
+
+    pub fn get_departure_string(data: &PublicTransportData, num_departures: usize) -> String {
+        let mut content = format!("{} -> {}", data.from.name, data.to.name);
+
+        if data.connections.is_empty() {
+            content += "\nNo departures";
+            return content;
+        }
+
+        let connections = data
+            .connections
+            .iter()
+            .filter(|connection| (connection.from.departure - MyWidget::now()).is_positive())
+            .take(num_departures);
+
+        for connection in connections {
+            let departure = connection.from.departure;
+            content += &format!(
+                "\n{} ({})",
+                MyWidget::format_departure_offset(departure),
+                MyWidget::format_departure(departure)
+            )
+            .to_string();
+        }
+        content
+    }
+
+    pub fn format_departure(departure: OffsetDateTime) -> String {
+        let format = format_description::parse("[hour]:[minute]").unwrap();
+        match departure.format(&format) {
+            Ok(departure) => departure,
+            Err(_) => "Could not format departure".to_string(),
+        }
+    }
+
+    pub fn format_departure_offset(departure: OffsetDateTime) -> String {
+        let departure_offset = departure - MyWidget::now();
+        HumanTime::from(departure_offset.unsigned_abs()).to_text_en(Accuracy::Rough, Tense::Future)
     }
 }
 
